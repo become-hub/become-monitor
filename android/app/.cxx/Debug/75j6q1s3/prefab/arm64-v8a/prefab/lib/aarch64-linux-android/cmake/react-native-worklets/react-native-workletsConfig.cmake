ee5e24ff367010664e4ba7360b5c1ced1f185235
Gabriele Muscogiuri <muscogiuri.gabriele@gmail.com>
2025-10-25 11:00:03 +0200

    Ignore all .cxx build dirs

--- android/app/.cxx/Debug/75j6q1s3/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/react-native-worklets/react-native-workletsConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET react-native-worklets::worklets)
-add_library(react-native-worklets::worklets SHARED IMPORTED)
-set_target_properties(react-native-worklets::worklets PROPERTIES
-    IMPORTED_LOCATION "/Users/gabriele/Git/become-monitor/node_modules/react-native-worklets/android/build/intermediates/cxx/Debug/4n2b6i2o/obj/arm64-v8a/libworklets.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/Users/gabriele/Git/become-monitor/node_modules/react-native-worklets/android/build/prefab-headers/worklets"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
--- android/app/.cxx/Debug/75j6q1s3/prefab/armeabi-v7a/prefab/lib/arm-linux-androideabi/cmake/fbjni/fbjniConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET fbjni::fbjni)
-add_library(fbjni::fbjni SHARED IMPORTED)
-set_target_properties(fbjni::fbjni PROPERTIES
-    IMPORTED_LOCATION "/Users/gabriele/.gradle/caches/8.14.3/transforms/9ee790c8cf11023d60d9d36b8fff1075/transformed/fbjni-0.7.0/prefab/modules/fbjni/libs/android.armeabi-v7a/libfbjni.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/Users/gabriele/.gradle/caches/8.14.3/transforms/9ee790c8cf11023d60d9d36b8fff1075/transformed/fbjni-0.7.0/prefab/modules/fbjni/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
